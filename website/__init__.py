fd814b6db128859aa71180599105a32ecf7d5473
RutujaBangera <152979568+RutujaBangera@users.noreply.github.com>
2025-03-12 11:50:27 +0530

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'SecretKey'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost:3306/bank'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root123@localhost:3306/bank'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
