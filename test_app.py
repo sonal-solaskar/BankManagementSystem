2229e02f67a17f7fab7a9db2554a684cdfa9ac9d
RutujaBangera <152979568+RutujaBangera@users.noreply.github.com>
2025-03-12 19:33:10 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 import pytest
 from flask import url_for
-from app import create_app, db
+from website import create_app, db
 from app.models import User
 from werkzeug.security import generate_password_hash
 
